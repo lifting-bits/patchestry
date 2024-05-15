e7cc65c6b87ed3dd5f17b7fe59eaa25c72ae83fb
Marek Surovič <marek.surovic@trailofbits.com>
2024-05-15 16:21:57 +0200

    pc: Adds basics of the `pc` pcode dialect.

---
+++ include/patchestry/Dialect/Pcode/PcodeOps.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <mlir/IR/RegionKindInterface.h>
+#include <mlir/Interfaces/SideEffectInterfaces.h>
+
+#include "patchestry/Dialect/Pcode/PcodeDialect.hpp"
+
+#define GET_OP_CLASSES
+#include "patchestry/Dialect/Pcode/Pcode.h.inc"
