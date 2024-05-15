7ab02e60cd7d70b23ed42c391afdfd7b03adea03
Marek Surovič <marek.surovic@trailofbits.com>
2024-05-15 16:21:57 +0200

    pc: Add basics of the `pc` pcode dialect.

---
+++ include/patchestry/Dialect/Pcode/PcodeDialect.hpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <mlir/IR/Dialect.h>
+
+// Pull in the dialect definition.
+#include "patchestry/Dialect/Pcode/PcodeDialect.h.inc"
---
+++ lib/patchestry/Dialect/Pcode/PcodeDialect.cpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2024, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#include "patchestry/Dialect/Pcode/PcodeDialect.hpp"
+#include "patchestry/Dialect/Pcode/PcodeOps.hpp"
+
+namespace patchestry::pc {
+    void PcodeDialect::initialize() {
+        addOperations<
+#define GET_OP_LIST
+#include "patchestry/Dialect/Pcode/Pcode.cpp.inc"
+            >();
+    }
+
+    using DialectParser  = mlir::AsmParser;
+    using DialectPrinter = mlir::AsmPrinter;
+
+} // namespace patchestry::pc
+
+#include "patchestry/Dialect/Pcode/PcodeDialect.cpp.inc"
