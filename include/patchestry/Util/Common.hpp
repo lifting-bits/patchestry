469acaef0bb97abb56bcb14642adf09aff671120
Lukas Korencik <lukas.korencik@trailofbits.com>
2024-06-26 15:36:26 +0200

    util: Add header with shared type aliases.

---
+++ include/patchestry/Util/Common.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "patchestry/Util/Warnings.hpp"
+
+PATCHESTRY_RELAX_WARNINGS
+#include <mlir/IR/Operation.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/Builders.h>
+PATCHESTRY_UNRELAX_WARNINGS
+
+namespace patchestry {
+    using mcontext_t = mlir::MLIRContext;
+
+    using mlir_operation = mlir::Operation *;
+    using mlir_type = mlir::Type;
+    using mlir_value = mlir::Value;
+
+    using mlir_builder = mlir::OpBuilder;
+
+    using loc_t = mlir::Location;
+
+} // namespace patchestry
