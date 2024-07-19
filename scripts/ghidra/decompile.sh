839357dd767ca98d15103f8da9ae5659177c4a92
Marek Surovič <marek.surovic@trailofbits.com>
2024-07-19 20:28:49 +0200

    ghidra: Extract JSON w/ pcode from the docker container.

--- scripts/ghidra/decompile.sh
+++ scripts/ghidra/decompile.sh
@@ -9,25 +9,26 @@
 
 set -x
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <file_path> [<function_name>]"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <input_file> <function_name> <output_file>"
     exit 1
 fi
 
-FILE_PATH=$1
+INPUT_PATH=$1
 FUNCTION_NAME=$2
+OUTPUT_PATH=$3
 
 # Create a new Ghidra project and import the file
 ${GHIDRA_HEADLESS} ${GHIDRA_PROJECTS} patchestry-decompilation \
     -readOnly -deleteProject \
-    -import $FILE_PATH \
+    -import $INPUT_PATH \
     -postScript PatchestryScript.java \
-    $FUNCTION_NAME
+    $FUNCTION_NAME \
+    $OUTPUT_PATH
+
 
 # Check if the decompile script was successful
 if [ $? -ne 0 ]; then
     echo "Decompilation failed"
     exit 1
 fi
-
-# TODO copy the decompiled output to the output directory
