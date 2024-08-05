50b0c2bd83e4567cae0a4432eda4eb1200afaf37
AkshayK <iit.akshay@gmail.com>
2024-08-05 10:45:43 -0400

    Fix directory permission issue on ubuntu

--- scripts/ghidra/decompile.sh
+++ scripts/ghidra/decompile.sh
@@ -18,6 +18,14 @@ INPUT_PATH=$1
 FUNCTION_NAME=$2
 OUTPUT_PATH=$3
 
+# Running with non-root user may cause permission issue on ubuntu
+# because binded directory will have root permission.
+# This is a hacky fix to avoid the issue. It can be avoided
+# by switching to using docker volume.
+if [ ! -w ${OUTPUT_PATH} ]; then
+  sudo chown ${USER}:${USER} ${OUTPUT_PATH}
+fi
+
 # Create a new Ghidra project and import the file
 ${GHIDRA_HEADLESS} ${GHIDRA_PROJECTS} patchestry-decompilation \
     -readOnly -deleteProject \
