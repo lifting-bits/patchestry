d0862545b77f1e9095e80cb9e6bb48e1649fac20
AkshayK <iit.akshay@gmail.com>
2024-08-08 15:03:13 -0400

    test: move test to ghidra script directory

---
+++ scripts/ghidra/test/src/argc.c
@@ -0,0 +1,7 @@
+int argc(int argc, char **argv) {
+    return argc;
+}
+int main(int a, char **argv)
+{
+    return argc(a, argv);
+}
---
+++ scripts/ghidra/test/src/union.c
@@ -0,0 +1,18 @@
+struct access
+{
+    int l;
+    int h;
+};
+
+union data
+{
+    unsigned long long b;
+    struct access s;
+};
+
+int main(int argc, char **argv)
+{
+    union data d;
+    d.b = 0xffffffff00000000 + argc;
+    return d.s.l;
+}
