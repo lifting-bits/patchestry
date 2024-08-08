39d87623640a50e2d0d301c5c033440c250d3ac2
AkshayK <iit.akshay@gmail.com>
2024-08-08 15:03:13 -0400

    test: move test to ghidra script directory

---
+++ scripts/ghidra/decompile-headless-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright (c) 2024, Trail of Bits, Inc.
+# All rights reserved.
+#
+# This source code is licensed in accordance with the terms specified in
+# the LICENSE file found in the root directory of this source tree.
+#
+SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [[ ! -d "${SCRIPTS_DIR}/test" ]]; then
+    echo "No tests found!!!" && exit 1
+fi
+
+cmake -B ${SCRIPTS_DIR}/build -S ${SCRIPTS_DIR}/test
+cmake --build ${SCRIPTS_DIR}/build
+ctest --test-dir ${SCRIPTS_DIR}/build
