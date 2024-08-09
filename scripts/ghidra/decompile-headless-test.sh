45d5425885cfeb3337f4356d93b78a892d40e6ca
AkshayK <iit.akshay@gmail.com>
2024-08-08 23:34:44 -0400

    test: remove llvm 18 dependencies from test framework

--- scripts/ghidra/decompile-headless-test.sh
+++ scripts/ghidra/decompile-headless-test.sh
@@ -8,14 +8,9 @@
 #
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CMAKE_ARGS=
-LLVM_DIR=$1
 
 if [[ ! -d "${SCRIPTS_DIR}/test" ]]; then
     echo "No tests found!!!" && exit 1
-fi
-
-if [ -n "${LLVM_DIR}" ]; then
-    CMAKE_ARGS="${CMAKE_ARGS} -DLLVM_DIR=${LLVM_DIR}"
 fi
 
 cmake ${CMAKE_ARGS} -B ${SCRIPTS_DIR}/build -S ${SCRIPTS_DIR}/test
