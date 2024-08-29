3760ea27fffc69bcacd112c423de949980e6a497
AkshayK <iit.akshay@gmail.com>
2024-08-29 09:19:35 -0400

    test: Update lit config to wrap decompiler-headless tool.

--- test/ghidra/argc.c
+++ test/ghidra/argc.c
@@ -1,8 +1,5 @@
 // UNSUPPORTED: system-windows
-// RUN: %cc %s -o %t
-// RUN %t; if [ "$(uname)" = "Linux" ]; then %decompile-headless %t argc %t1 fi
-// RUN %t; if [ "$(uname)" = "Darwin" ]; then %decompile-headless %t _argc %t1 fi
-// RUN %t1; %file-check %s --input-file %t1
+// RUN: %cc %s -o %t && %decompile-headless %t argc %t1 && %file-check -vv %s --input-file %t1
 // CHECK: {{...}}
 
 int argc(int argc, char **argv) {
--- test/ghidra/insert.c
+++ test/ghidra/insert.c
@@ -1,8 +1,5 @@
 // UNSUPPORTED: system-windows
-// RUN: %cc %s -o %t
-// RUN %t; if [ "$(uname)" = "Linux" ]; then %decompile-headless %t insert_substring %t1 fi
-// RUN %t; if [ "$(uname)" = "Darwin" ]; then %decompile-headless %t _insert_substring %t1 fi
-// RUN %t1; %file-check %s --input-file %t1
+// RUN: %cc %s -o %t && %decompile-headless %t insert_substring %t1 && %file-check %s --input-file %t1
 // CHECK: {{...}}
 
 #include <stdio.h>
