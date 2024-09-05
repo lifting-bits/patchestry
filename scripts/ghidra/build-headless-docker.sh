bfb713a5ba0ca77c83315bbdafcf8efd51f685aa
xlauko <xlauko@mail.muni.cz>
2024-09-05 13:06:25 +0200

    treewide: Refactor headless container tooling and scripts.

--- scripts/ghidra/build-headless-docker.sh
+++ scripts/ghidra/build-headless-docker.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Get the directory of the current script
 SCRIPTS_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-# Print the script directory (for debugging purposes, can be removed)
 echo "Using SCRIPTS_DIR: $SCRIPTS_DIR"
 
-# Build the Docker image
 docker build \
     -t trailofbits/patchestry-decompilation:latest \
     -f "${SCRIPTS_DIR}/decompile-headless.dockerfile" \
     "${SCRIPTS_DIR}"
 
-# Check if the Docker build was successful
 if [ $? -eq 0 ]; then
     echo "Docker image built successfully."
 else
