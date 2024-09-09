d68b380c303dcd68b7af52b658017527cd01f3c8
xlauko <xlauko@mail.muni.cz>
2024-09-09 16:52:07 +0200

    ghidra: Remove obsolete codegen and prepare pcode translation registration.

---
+++ lib/patchestry/Ghidra/PcodeTranslation.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024, Trail of Bits, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#include <patchestry/Ghidra/PcodeTranslation.hpp>
+
+namespace patchestry::ghidra {
+
+    void register_pcode_translation() {
+
+    }
+
+} // namespace patchestry::ghidra
