43670efd02d88cf0c225bc7f0f6754a20bf0269d
xlauko <xlauko@mail.muni.cz>
2024-09-09 17:17:22 +0200

    ghidra: Register pcode translation.

--- lib/patchestry/Ghidra/PcodeTranslation.cpp
+++ lib/patchestry/Ghidra/PcodeTranslation.cpp
@@ -8,10 +8,36 @@
 
 #include <patchestry/Ghidra/PcodeTranslation.hpp>
 
+#include <patchestry/Dialect/Pcode/PcodeDialect.hpp>
+
+#include <patchestry/Util/Common.hpp>
+
+#include <mlir/IR/OwningOpRef.h>
+#include <mlir/Tools/mlir-translate/Translation.h>
+
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/SourceMgr.h>
+
 namespace patchestry::ghidra {
 
-    void register_pcode_translation() {
+    static mlir::OwningOpRef< mlir_operation > deserialize(
+        const llvm::MemoryBuffer *buffer, mcontext_t * /* mctx */
+    ) {
+        return {};
+    }
 
+    void register_pcode_translation() {
+        mlir::TranslateToMLIRRegistration(
+            "deserialize-pcode", "translate Ghidra Pcode JSON into Patchestry's Pcode dialect",
+            [] (llvm::SourceMgr &smgr, mcontext_t *mctx) {
+                assert(smgr.getNumBuffers() == 1 && "expected one buffer");
+                smgr.
+                return deserialize(smgr.getMemoryBuffer(smgr.getMainFileID()), mctx);
+            },
+            [] (mlir::DialectRegistry &registry) {
+                registry.insert< patchestry::pc::PcodeDialect >();
+            }
+        );
     }
 
 } // namespace patchestry::ghidra
