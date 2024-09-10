0ef5ebfdf95f23c27f6cf39076a8315892abc17c
xlauko <xlauko@mail.muni.cz>
2024-09-10 09:39:52 +0200

    test: Setup pcode-translate testing.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -21,7 +21,7 @@ config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
 config.python_executable = config.python_executable if config.python_executable else sys.executable
 
 # Define file suffixes for test files
-config.suffixes = ['.c', '.cpp']
+config.suffixes = ['.c', '.cpp', '.json']
 
 # Set the root directory where tests should be executed
 config.test_exec_root = os.path.join(config.patchestry_obj_root, 'test')
@@ -31,11 +31,22 @@ config.test_source_root = os.path.join(config.patchestry_src_root, 'test')
 
 # Set the directory for Ghidra scripts
 config.patchestry_script_dir = os.path.join(config.patchestry_src_root, 'scripts', 'ghidra')
+config.patchestry_tools_dir = os.path.join(config.patchestry_obj_root, 'tools')
 
 # Add the Ghidra scripts directory to the substitutions list
 config.substitutions.append(('%PATH%', config.patchestry_script_dir))
 
+if 'BUILD_TYPE' in lit_config.params:
+    config.patchestry_build_type = lit_config.params['BUILD_TYPE']
+else:
+    config.patchestry_build_type = "Debug"
+
+def patchestry_tool_path(tool):
+    path = [config.patchestry_tools_dir, tool, config.patchestry_build_type]
+    return os.path.join(*path, tool)
+
 config.decompiler_headless_tool = os.path.join(config.patchestry_script_dir, 'decompile-headless.sh')
+config.pcode_translate_tool = patchestry_tool_path('pcode-translate')
 
 # Define tool substitutions
 tools = [
@@ -44,8 +55,10 @@ tools = [
     ToolSubst('%cxx', command=FindTool('clang++')),
     ToolSubst('%host_cc', command=config.host_cc),
     ToolSubst('%host_cxx', command=config.host_cxx),
-    ToolSubst('%decompile-headless', command=config.decompiler_headless_tool)
+    ToolSubst('%decompile-headless', command=config.decompiler_headless_tool),
+    ToolSubst('%pcode-translate', command=config.pcode_translate_tool),
 ]
+
 
 # Process tool substitutions
 for tool in tools:
