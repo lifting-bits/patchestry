01d7afb8424f3049d5de6ed77061b03033823492
Peter Goodman <peter.goodman@gmail.com>
2024-11-04 18:40:47 -0500

    treewide: Fix license block.

--- scripts/ghidra/decompile-entrypoint.sh
+++ scripts/ghidra/decompile-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 #
 # Copyright (c) 2024, Trail of Bits, Inc.
-# All rights reserved.
 #
 # This source code is licensed in accordance with the terms specified in
 # the LICENSE file found in the root directory of this source tree.
