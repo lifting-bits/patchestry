324cb7017b955eafccef8b49ae0da577b4167b52
Peter Goodman <peter.goodman@gmail.com>
2024-11-08 17:45:38 -0500

    ghidra: Recover globals and render them in the DOT digraph.

--- scripts/render_json.py
+++ scripts/render_json.py
@@ -67,49 +67,52 @@ def render_typed_var(var: str, type_key: str, types: Dict[str, Dict]) -> str:
             return var
 
 
-def render_output(data: Dict[str, str], operations: Dict[str, Dict]):
+def render_output(data: Dict[str, str], operations: Dict[str, Dict], vars: Dict[str, Dict]):
     assert "kind" in data
     if data["kind"] == "global":
-        print("?GLOBAL?", end=" = ")
+        print(var_name(vars, data), end=" = ")
     else:
         assert "operation" in data
         data = operations[data["operation"]]
         assert data["mnemonic"].startswith("DECLARE_")
-        print(var_name(data), end=" = ")
+        print(var_name(vars, data), end=" = ")
 
 
-def render_input(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], types: Dict[str, Dict]):
+def render_input(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], vars: Dict[str, Dict], types: Dict[str, Dict]):
     match data["kind"]:
         case "constant":
             print(data["value"], end='')
         case "temporary":
             source = operations[data["operation"]]
             if "name" in source:
-                print(var_name(source), end='')
+                print(var_name(vars, source), end='')
             else:
                 print('(', end='')
-                render_op(source, operations, functions, types, True)
+                render_op(source, operations, functions, vars, types, True)
                 print(')', end='')
         case "local":
-            print(var_name(operations[data["operation"]]), end='')
+            print(var_name(vars, operations[data["operation"]]), end='')
         case "parameter":
-            print(var_name(operations[data["operation"]]), end='')
+            print(var_name(vars, operations[data["operation"]]), end='')
         case "global":
-            print("?GLOBAL?", end='')
+            print(var_name(vars, data), end='')
         case "function":
             print(functions[data["function"]]["name"], end='')
         case _:
             print("?INPUT?", end='')
 
 
-def var_name(data: Dict) -> str:
+def var_name(vars: Dict[str, Dict], data: Dict) -> str:
+    if data["kind"] == "global":
+        return vars[data["global"]]["name"]
+
     name = data["name"]
     if data["kind"] == "temporary":
         name += "_" + str(ID[data["address"]])
     return name
 
 
-def render_op(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], types: Dict[str, Dict], inline=False):
+def render_op(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], vars: Dict[str, Dict], types: Dict[str, Dict], inline=False):
     if not should_render(data):
         if not inline:
             return
@@ -119,16 +122,16 @@ def render_op(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict
         if inline:
             print(data["name"], end='')
         else:
-            decl = render_typed_var(" " + var_name(data), data["type"], types)
+            decl = render_typed_var(" " + var_name(vars, data), data["type"], types)
             print(f"{decl}<BR />", end='')
         return
 
     is_call = mnemonic in ("CALL", "CALLIND")
     if "output" in data:
-        render_output(data["output"], operations)
+        render_output(data["output"], operations, vars)
 
     if is_call:
-        render_input(data["target"], operations, functions, types)
+        render_input(data["target"], operations, functions, vars, types)
         print("(")
     else:
         print(mnemonic, end=" ")
@@ -138,11 +141,11 @@ def render_op(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict
     if "inputs" in data:
         for input in data["inputs"]:
             print("", end=sep)
-            render_input(input, operations, functions, types)
+            render_input(input, operations, functions, vars, types)
             sep = ", "
 
     if "condition" in data:  # For a CBRANCH
-        render_input(data["condition"], operations, functions, types)
+        render_input(data["condition"], operations, functions, vars, types)
 
     if is_call:
         print(")")
@@ -151,12 +154,13 @@ def render_op(data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict
         print("<BR />", end='')
 
 
-def render_block(key: int, data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], types: Dict[str, Dict]):
+def render_block(key: int, data: Dict, operations: Dict[str, Dict], functions: Dict[str, Dict], vars: Dict[str, Dict], types: Dict[str, Dict]):
     print(f"b{key} [label=<<TABLE cellpadding=\"0\" cellspacing=\"0\" border=\"1\" align=\"left\"><TR><TD align=\"left\">", end='')
     last_op: Optional[Dict] = None
     for op_key in data["ordered_operations"]:
         last_op = operations[op_key]
-        render_op(last_op, operations, functions, types)
+        render_op(last_op, operations, functions, vars, types)
+
     print(f"</TD></TR></TABLE>>];")
 
     if last_op is None:
@@ -175,7 +179,7 @@ def render_block(key: int, data: Dict, operations: Dict[str, Dict], functions: D
         print(f"b{key} -> b{target_key} [color=\"green\"];")
 
 
-def render_function(func_key: str, functions: Dict[str, Dict], types: Dict[str, Dict]):
+def render_function(func_key: str, functions: Dict[str, Dict], vars: Dict[str, Dict], types: Dict[str, Dict]):
     data = functions[func_key]
     key = ID[func_key]
     basic_blocks: Dict = data.get("basic_blocks", EMPTY)
@@ -210,15 +214,16 @@ def render_function(func_key: str, functions: Dict[str, Dict], types: Dict[str, 
     print(f"f{key} -> b{entry_block_key};")
 
     for block_key, block in basic_blocks.items():
-        render_block(ID[block_key], block, operations, functions, types)
+        render_block(ID[block_key], block, operations, functions, vars, types)
 
 
 def render_functions(data: Dict):
     print("digraph {")
     print("node [shape=none fontname=Courier];")
     functions: Dict[str, Dict] = data["functions"]
+    vars: Dict[str, Dict] = data["globals"]
     for func_key in functions.keys():
-        render_function(func_key, functions, data["types"])
+        render_function(func_key, functions, vars, data["types"])
     print("}")
 
 
