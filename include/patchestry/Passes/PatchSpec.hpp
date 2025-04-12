96ad36bfc0e2fa7c65aa611d7f68b15519d71066
AkshayK <iit.akshay@gmail.com>
2025-04-11 21:51:21 -0400

    translate: update specification parser for patches

---
+++ include/patchestry/Passes/PatchSpec.hpp
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) 2025, Trail of Bits, Inc.
+ *
+ * This source code is licensed in accordance with the terms specified in
+ * the LICENSE file found in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <llvm/Support/YAMLTraits.h>
+
+namespace patchestry::passes {
+
+    enum class PatchOperationKind : uint8_t {
+        NONE = 0, // No patch
+        APPLY_BEFORE_PATCH,
+        APPLY_AFTER_PATCH,
+        WRAP_AROUND_PATCH
+    };
+
+    struct PatchOperation
+    {
+        PatchOperationKind kind = PatchOperationKind::NONE;
+        std::string target;
+        std::vector< std::string > arguments;
+        std::string return_value;
+    };
+
+    struct PatchConfig
+    {
+        std::string function;
+        std::string patch_file;
+        std::optional< std::string > patch_module;
+        std::vector< PatchOperation > operations;
+    };
+
+    struct PatchSpec
+    {
+        std::string arch;
+        std::vector< PatchConfig > patches;
+    };
+
+}; // namespace patchestry::passes
+
+LLVM_YAML_IS_SEQUENCE_VECTOR(patchestry::passes::PatchConfig)
+
+namespace llvm::yaml {
+
+    // Prase PatchOperation
+    template<>
+    struct MappingTraits< patchestry::passes::PatchOperation >
+    {
+        static void mapping(IO &io, patchestry::passes::PatchOperation &arg) {
+            io.mapRequired("target", arg.target);
+            io.mapOptional("arguments", arg.arguments);
+            io.mapOptional("return_value", arg.return_value);
+        }
+    };
+
+    // Prase PatchConfig
+    template<>
+    struct MappingTraits< patchestry::passes::PatchConfig >
+    {
+        static void mapping(IO &io, patchestry::passes::PatchConfig &spec) {
+            io.mapRequired("function", spec.function);
+            io.mapOptional("patch_file", spec.patch_file);
+
+            patchestry::passes::PatchOperation before_operation;
+            patchestry::passes::PatchOperation after_operation;
+            patchestry::passes::PatchOperation wrap_operation;
+
+            io.mapOptional("apply_before", before_operation);
+            io.mapOptional("apply_after", after_operation);
+            io.mapOptional("wrap_around", wrap_operation);
+            bool has_apply_before = before_operation.target != "";
+            bool has_apply_after  = after_operation.target != "";
+            bool has_wrap_around  = wrap_operation.target != "";
+
+            if ((has_apply_before || has_apply_after) && has_wrap_around) {
+                llvm::report_fatal_error(
+                    "Wrap around patch cannot be combined with apply before or after patch"
+                );
+            }
+
+            if (has_apply_before) {
+                before_operation.kind =
+                    patchestry::passes::PatchOperationKind::APPLY_BEFORE_PATCH;
+                spec.operations.emplace_back(before_operation);
+            } else if (has_apply_after) {
+                after_operation.kind =
+                    patchestry::passes::PatchOperationKind::APPLY_AFTER_PATCH;
+                spec.operations.emplace_back(after_operation);
+            } else if (has_wrap_around) {
+                wrap_operation.kind = patchestry::passes::PatchOperationKind::WRAP_AROUND_PATCH;
+                spec.operations.emplace_back(wrap_operation);
+            }
+
+            // If no operations were defined, report an error
+            if (spec.operations.empty()) {
+                std::string error_message =
+                    "No patch operations specified for function: " + spec.function;
+                llvm::report_fatal_error(error_message.c_str());
+            }
+        }
+    };
+
+    // Prase PatchSpec
+    template<>
+    struct MappingTraits< patchestry::passes::PatchSpec >
+    {
+        static void mapping(IO &io, patchestry::passes::PatchSpec &config) {
+            io.mapRequired("arch", config.arch);
+            io.mapRequired("patches", config.patches);
+        }
+    };
+
+} // namespace llvm::yaml
