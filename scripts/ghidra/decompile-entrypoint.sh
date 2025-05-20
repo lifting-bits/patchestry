ad991184ec072537f0c37501c6a36a1cbbac3017
Kelly Kaoudis <kelly.kaoudis@trailofbits.com>
2025-05-20 15:48:29 +0000

    fixup: generally fix quoting and weirdness

--- scripts/ghidra/decompile-entrypoint.sh
+++ scripts/ghidra/decompile-entrypoint.sh
@@ -133,80 +133,79 @@ ARCHITECTURE=""
 # originally compiled with clang for linux, you'd still put gcc or default for
 # the variant string.
 function detect_processor {
-    local file_output = $(file "$INPUT_FILE")
+    local file_output=$(file "${INPUT_FILE}")
     local variant="default" 
     local mode="32" 
 
     local endianness="LE"
-    if [["$file_output" == *"big-endian"*]]; then
+    if [[ "$file_output" == *"big-endian"* ]]; then
         endianness="BE"
     fi
 
     if [[ "$file_output" == *"ELF"* ]]; then
-      variant="gcc"
-    elif [[ "$file_output" == *"PE32"* || "$file_output" == *"PE32+"* ]]; then
+      variant="default" #gcc isn't supported for now
+    elif [[ "$file_output" == *"PE32"* ]]; then
       variant="windows"
     elif [[ "$file_output" == *"Mach-O"* ]]; then
       variant="clang"
     fi 
 
-    local processor_name=$("$file_output" | grep -o -E 'x86-64|Intel 80386|ARMv[0-9]+|armv[0-9]+|ARM aarch64|AArch64|ARM, big-endian|ARM, little-endian|ARM')
-    if [[ "$processor_name" =~ "x86-64" ]]; then
+    local processor_name=$(echo "$file_output" | grep -o -E 'x86-64|Intel 80386|ARMv[0-9]+|armv[0-9]+|ARM aarch64|AArch64|ARM, big-endian|ARM, little-endian|ARM')
+    if [[ "$processor_name" =~ x86-64 ]]; then
         ARCHITECTURE="x86:${endianness}:64:${variant}"
         return
-    elif [[ "$processor_name" =~ "Intel 80386" ]]; then
-        ARCHITECTURE="x86:${endianness}:{$mode}:${variant}"
+    elif [[ "$processor_name" =~ Intel\ 80386 ]]; then
+        ARCHITECTURE="x86:${endianness}:${mode}:${variant}"
         return
-    elif [[ "$processor_name" =~ "ARM aarch64|AArch64" ]]; then
+    elif [[ "$processor_name" =~ ARM\ aarch64|AArch64 ]]; then
         ARCHITECTURE="AARCH64:${endianness}:64:v8A"
         return
-    fi
-
-    if [[ "$processor_name" =~ ARM || "$processor_name" =~ armv ]]; then
+    elif [[ "$processor_name" =~ ARM|armv ]]; then
         variant="v7"
-        if readelf -S "$INPUT_FILE" | grep -q -E "\.text.*08000000|\.data.*20000000"; then
-            # Cortex-M memory layout detected
-            ARCHITECTURE="ARM:${endianness}:${mode}:${variant}"
-        else
-            # Check ARM attributes for version
-            local arm_attrs=$(readelf -A "$INPUT_FILE" 2>/dev/null)
-            if echo "$arm_attrs" | grep -q "Tag_CPU_arch: v8"; then
-                ARCHITECTURE="ARM:${endianness}:${mode}:v8"
-            elif echo "$arm_attrs" | grep -q "Tag_CPU_arch: v7"; then
-                if [["$arm_attrs" =~ "Cortex-M"]]; then
-                  variant="Cortex"
-                fi
-
-                ARCHITECTURE="ARM:${endianness}:${mode}:${variant}"
-            elif echo "$arm_attrs" | grep -q "Tag_CPU_arch: v6"; then
-                ARCHITECTURE="ARM:${endianness}:${mode}:v6"
-            elif echo "$arm_attrs" | grep -q "Tag_CPU_arch: v5"; then
-                ARCHITECTURE="ARM:${endianness}:${mode}:v5"
-            elif echo "$arm_attrs" | grep -q "Tag_CPU_arch: v4"; then
-                ARCHITECTURE="ARM:${endianness}:${mode}:v4"
-            else
-                echo "Not sure what we have here... defaulting to ARMv7 for now... Attributes: '${arm_attrs}'"
-                ARCHITECTURE="ARM:${endianness}:${mode}:${variant}"
+        local arm_attrs=$(readelf -A "$INPUT_FILE" 2>/dev/null)
+        if [[ "$arm_attrs" == *"Tag_CPU_arch: v8"* ]]; then
+            ARCHITECTURE="ARM:${endianness}:${mode}:v8"
+        elif [[ "$arm_attrs" == *"Tag_CPU_arch: v7"* ]]; then
+            if [[ "$arm_attrs" =~ "Cortex-M" ]] || readelf -S "$INPUT_FILE" | grep -q -E "\.text.*08000000|\.data.*20000000"; then
+              variant="Cortex"
             fi
+
+            ARCHITECTURE="ARM:${endianness}:${mode}:${variant}"
+        elif [[ "$arm_attrs" == *"Tag_CPU_arch: v6"* ]]; then
+            ARCHITECTURE="ARM:${endianness}:${mode}:v6"
+        elif [[ "$arm_attrs" == *"Tag_CPU_arch: v5"* ]]; then
+            ARCHITECTURE="ARM:${endianness}:${mode}:v5"
+        elif [[ "$arm_attrs" == *"Tag_CPU_arch: v4"* ]]; then
+            ARCHITECTURE="ARM:${endianness}:${mode}:v4"
+        else
+            echo "Not sure what we have here... defaulting to ARMv7 for now... Attributes: '${arm_attrs}'"
+            ARCHITECTURE="ARM:${endianness}:${mode}:${variant}"
         fi
     else
         echo "Could not determine full input-binary architecture"
-        observed '$processor_name':'$endianness':'$mode':'$variant'
+        echo "Observed '$processor_name':'$endianness':'$mode':'$variant'"
         exit 1
     fi
 
-    echo "observed '$processor_name':'$endianness':'$mode':'$variant'"
+    echo "Observed '$processor_name':'$endianness':'$mode':'$variant'"
 }
 
 # Function to run Ghidra headless script for listing functions
 function run_list_functions {
   echo "Running Ghidra headless script to list functions..."
+
+  local guess_architecture=""
+  if [[ -n "$ARCHITECTURE" ]]; then
+    guess_architecture="-processor ${ARCHITECTURE}"
+  fi
+  
   ${GHIDRA_HEADLESS} ${GHIDRA_PROJECTS} patchestry-decompilation \
-    -readOnly -deleteProject \
-    -import $INPUT_FILE \
-    -processor $ARCHITECTURE \
+    -readOnly \
+    -deleteProject \
+    -import ${INPUT_FILE} \
+    $guess_architecture\
     -postScript "PatchestryListFunctions.java" \
-    $OUTPUT_FILE
+    ${OUTPUT_FILE}
 
   if [ $? -ne 0 ]; then
     die "List function failed"
@@ -215,18 +214,23 @@ function run_list_functions {
 
 # Function to run Ghidra headless script for decompiling
 function run_decompile_single {
-  echo "Running Ghidra headless script to decompile function..."
-  local ghidra_script="PatchestryDecompileFunctions.java"
+  echo "Running Ghidra headless script to decompile the single function '$FUNCTION_NAME'..."
+
+  local guess_architecture=""
+  if [[ -n "$ARCHITECTURE" ]]; then
+    guess_architecture="-processor ${ARCHITECTURE}"
+  fi
   
   ${GHIDRA_HEADLESS} ${GHIDRA_PROJECTS} patchestry-decompilation \
-    -readOnly -deleteProject \
-    -import $INPUT_FILE \
-    -processor $ARCHITECTURE \
-    -postScript ${ghidra_script} \
+    -readOnly \
+    -deleteProject \
+    -import ${INPUT_FILE} \
+    $guess_architecture\
+    -postScript "PatchestryDecompileFunctions.java" \
     single \
-    $FUNCTION_NAME \
-    $OUTPUT_FILE
-
+    ${FUNCTION_NAME} \
+    ${OUTPUT_FILE}
+   
   if [ $? -ne 0 ]; then
     die "Decompilation failed"
   fi
@@ -234,16 +238,21 @@ function run_decompile_single {
 
 function run_decompile_all {
   echo "Running Ghidra headless script to decompile all functions..."
-  local ghidra_script="PatchestryDecompileFunctions.java"
-
+  
+  local guess_architecture=""
+  if [[ -n "$ARCHITECTURE" ]]; then
+    guess_architecture="-processor ${ARCHITECTURE}"
+  fi
+  
   ${GHIDRA_HEADLESS} ${GHIDRA_PROJECTS} patchestry-decompilation \
-    -readOnly -deleteProject \
-    -import $INPUT_FILE \
-    -processor $ARCHITECTURE \
-    -postScript ${ghidra_script} \
+    -readOnly \
+    -deleteProject \
+    -import ${INPUT_FILE} \
+    ${guessArchitecture}\
+    -postScript "PatchestryDecompileFunctions.java" \
     all \
-    $OUTPUT_FILE
-
+    ${OUTPUT_FILE}
+ 
   if [ $? -ne 0 ]; then
     die "Decompilation failed"
   fi
