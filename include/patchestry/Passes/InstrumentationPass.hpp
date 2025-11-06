dfeab9420a5bbbf86c39d61f8cd60b7f2c4bf542
AkshayK <iit.akshay@gmail.com>
2025-11-06 14:13:11 -0500

    transform: add support for static contracts and refactor instrumentation code

--- include/patchestry/Passes/InstrumentationPass.hpp
+++ include/patchestry/Passes/InstrumentationPass.hpp
@@ -34,7 +34,11 @@ namespace mlir {
 
 namespace patchestry::passes { // NOLINT
 
+    class ContractOperationImpl;
+    class PatchOperationImpl;
+
     struct InstrumentationOptions;
+    struct ContractInformation;
 
     /**
      * @brief Registers instrumentation passes with the MLIR pass registry.
@@ -79,12 +83,6 @@ namespace patchestry::passes { // NOLINT
         std::optional< patch::PatchAction > patch_action;
     };
 
-    struct ContractInformation
-    { // the use of optional here takes care of some typing errors
-        std::optional< contract::ContractSpec > spec;
-        std::optional< contract::ContractAction > action;
-    };
-
     /**
      * @brief MLIR pass that applies code instrumentation based on configuration.
      *
@@ -105,6 +103,9 @@ namespace patchestry::passes { // NOLINT
         : public mlir::PassWrapper< InstrumentationPass, mlir::OperationPass< mlir::ModuleOp > >
 
     {
+        friend class ContractOperationImpl;
+        friend class PatchOperationImpl;
+
         /** @brief Path to the YAML Patchestry configuration file */
         std::string configuration_file;
 
@@ -112,7 +113,7 @@ namespace patchestry::passes { // NOLINT
         std::optional< Configuration > config;
 
         /** @brief List of operations to be inlined after instrumentation */
-        std::vector< mlir::Operation * > inline_worklists;
+        std::set< mlir::Operation * > inline_worklists;
 
         /** @brief Reference to inlining configuration options */
         const InstrumentationOptions &options;
